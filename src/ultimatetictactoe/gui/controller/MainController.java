0aa3bef095e6d52c83f606c4478888c83b04ef17
Rado <radoslav.backovsky@gmail.com>
2020-02-26 13:10:11 +0100

    button width and height constants

--- src/ultimatetictactoe/gui/controller/MainController.java
+++ src/ultimatetictactoe/gui/controller/MainController.java
@@ -29,6 +29,8 @@ public class MainController implements Initializable {
     @FXML
     private AnchorPane mainPane;
     private UTTTButton btn;
+    private final int BTN_WIDTH = 40;
+    private final int BTN_HEIGHT = 40;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -40,16 +42,16 @@ public class MainController implements Initializable {
     }
 
     private void createTiles() {
-        int btnWidth = 40;
-        int btnHeight = 40;
         for (int x = 0; x < 9; x++) {
             for (int y = 0; y < 9; y++) {
 
                 btn = new UTTTButton();
-                btn.setPrefSize(btnWidth, btnHeight);
+                btn.setPrefSize(BTN_WIDTH, BTN_HEIGHT);
+
                 btn.setMove(new Move(x, y));
-                btn.setLayoutX(20 + (btnWidth + 5) * x);
-                btn.setLayoutY(20 + (btnHeight + 5) * y);
+
+                btn.setLayoutX(20 + (BTN_WIDTH + 5) * x);
+                btn.setLayoutY(20 + (BTN_HEIGHT + 5) * y);
                 play();
                 mainPane.getChildren().add(btn);
             }
