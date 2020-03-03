13f684858adbeddfcd9b26142bbc003ee6d18ba2
Francesco <fran131b@easv365.dk>
2020-03-03 15:59:33 +0100

    first window fix

--- src/ultimatetictactoe/Main.java
+++ src/ultimatetictactoe/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/ultimatetictactoe/gui/view/mainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/ultimatetictactoe/gui/view/FirstWindow.fxml"));
         
         Scene scene = new Scene(root);
         
--- src/ultimatetictactoe/gui/controller/FirstWindowController.java
+++ src/ultimatetictactoe/gui/controller/FirstWindowController.java
@@ -3,13 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ultimatetictactoe.gui.view;
+package ultimatetictactoe.gui.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -28,5 +34,14 @@ public class FirstWindowController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
       lbl_title.setId("lbl_title");
     }    
+    @FXML
+    private void clickStartGame (ActionEvent event) throws IOException{
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("/ultimatetictactoe/gui/view/mainWindow.fxml"));
+       Parent root=loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }
