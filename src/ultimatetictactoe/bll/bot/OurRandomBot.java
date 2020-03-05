462da553ae490efaedfca092d69d96debe9c1dbc
Francesco <fran131b@easv365.dk>
2020-03-05 22:14:16 +0100

    github repo added to bot

--- src/ultimatetictactoe/bll/bot/OurRandomBot.java
+++ src/ultimatetictactoe/bll/bot/OurRandomBot.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Link to Github Repository
+//https://github.com/mrbacky/UltimateTicTacToe
 package ultimatetictactoe.bll.bot;
 
 import java.util.Random;
